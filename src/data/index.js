dbf96825a8d2747fa8e432d77805579d187c96b5
davidolaoluwa240 <davidolaoluwa240@gmail.com>
2023-04-23 00:26:23 +0200

    Done with the resources_items data

--- src/data/index.js
+++ src/data/index.js
@@ -192,235 +192,279 @@ export const RESOURCES_ITEMS = [
     content: "Complex Application Design: A 5-Layer Framework by NNGroup",
     link: "https://www.nngroup.com/articles/complex-application-design-framework/",
   },
-  { id: 4, content: "Choosing Your Journey by MJ Blehart", link: "" },
+  {
+    id: 4,
+    content: "Choosing Your Journey by MJ Blehart",
+    link: "https://medium.com/the-ascent/choosing-your-journey-1eaf83a31921",
+  },
   {
     id: 5,
     content:
       "A Customer Journey Map of the ProductPlan First Time User Experience by Jeremy Rawson",
-    link: "",
+    link: "https://www.productplan.com/customer-journey-map-first-time-user-experience/",
   },
-  { id: 6, content: "Describing Personas by Indi Young", link: "" },
+  {
+    id: 6,
+    content: "Describing Personas by Indi Young",
+    link: "https://medium.com/inclusive-software/describing-personas-af992e3fc527",
+  },
   {
     id: 7,
     content: "Design’s Unsexy Middle Bits by Christina Wodtke",
-    link: "",
+    link: "https://cwodtke.medium.com/designs-unsexy-middle-bits-a8cc17f0246d",
   },
   {
     id: 8,
     content: "Designing emotional micro-interactions by Antony Ribot",
-    link: "",
+    link: "https://ribot.co.uk/stories/designing-emotional-micro-interactions",
   },
   {
     id: 9,
     content: "Designing Services That Deliver by G. Lynn Shostack",
-    link: "",
+    link: "https://hbr.org/1984/01/designing-services-that-deliver",
   },
   {
     id: 10,
     content:
       "Digital Transformation Is Not About Technology by Behnam Tabrizi , Ed Lam , Kirk Girard and Vernon Irvin",
-    link: "",
+    link: "https://hbr.org/2019/03/digital-transformation-is-not-about-technology",
   },
   {
     id: 11,
     content:
       "Escape Velocity: Finding Our Path in the Next Decade by Molly Stevens",
-    link: "",
+    link: "https://medium.com/@mollystevens/escape-velocity-finding-our-path-in-the-next-decade-2fb2a3846c1e",
   },
   {
     id: 12,
     content: "Five Habits of Design Thinking by Christina Wodtke",
-    link: "",
+    link: "http://eleganthack.com/five-habits-of-design-thinking-2/",
   },
   {
     id: 13,
     content:
       "Foundations to build on: Intercom’s principles for building product by Des Taylor",
-    link: "",
+    link: "https://www.intercom.com/blog/intercom-product-principles/",
   },
   {
     id: 14,
     content: "Generating Focused Ideas Through Practical Empathy by Indi Young",
-    link: "",
+    link: "https://medium.com/inclusive-software/generating-focused-ideas-through-practical-empathy-7a31092bf0f9",
   },
   {
     id: 15,
     content: "Good writing is good product design by Jordan Jackson",
-    link: "",
+    link: "https://foundationsoform.substack.com/p/good-writing-is-good-product-design",
   },
-  { id: 16, content: "Hazard Mapping by Erika Hall", link: "" },
+  {
+    id: 16,
+    content: "Hazard Mapping by Erika Hall",
+    link: "https://medium.com/mule-design/hazard-mapping-e0b99b7ebd29",
+  },
   {
     id: 17,
     content:
       "How to partner with UX researchers to positively impact product and business by Tatiana Vlahovic",
-    link: "",
+    link: "https://uxdesign.cc/how-to-partner-with-user-experience-researchers-to-positively-impact-product-and-business-a82580fc6ea5",
   },
-  { id: 18, content: "How to Wield Empathy by Indi Young", link: "" },
+  {
+    id: 18,
+    content: "How to Wield Empathy by Indi Young",
+    link: "https://medium.com/inclusive-software/describing-personas-af992e3fc527",
+  },
   {
     id: 19,
     content:
       "If time travel were possible, what would I tell myself as a junior UX researcher? by Mark",
-    link: "",
+    link: "https://medium.com/cheggux/if-time-travel-were-possible-what-would-i-tell-myself-as-a-junior-ux-researcher-30c5dfd7dc70",
   },
-  { id: 20, content: "Jobs to be done personas by Nikki Anderson", link: "" },
+  {
+    id: 20,
+    content: "Jobs to be done personas by Nikki Anderson",
+    link: "https://uxdesign.cc/jobs-to-be-done-personas-5faa0ea3d438",
+  },
   {
     id: 21,
     content:
       "Jobs To Be Done: An Occasionally Useful UX Gimmick by Jared Spool",
-    link: "",
+    link: "https://articles.uie.com/jobs-to-be-done-an-occasionally-useful-ux-gimmick/",
   },
   {
     id: 22,
     content:
       "Know Your Customers’ “Jobs to Be Done” by Clayton M. Christensen , Taddy Hall , Karen Dillon and David S. Duncan",
-    link: "",
+    link: "https://hbr.org/2016/09/know-your-customers-jobs-to-be-done",
   },
   {
     id: 23,
     content:
       "The Ultimate Guide to Mixed Methods User Research by Ben Wiedmaier",
-    link: "",
+    link: "https://dscout.com/people-nerds/mixed-methods-research",
   },
   {
     id: 24,
     content: "My UX Research toolbox as a team of one by Lindsay Boylan",
-    link: "",
+    link: "https://uxdesign.cc/my-ux-research-toolbox-as-a-team-of-one-a7aa5041051d",
   },
   {
     id: 25,
     content: "Needfinding for Disruptive Innovation by Christina Wotdke",
-    link: "",
+    link: "https://medium.com/the-creative-founder/needfinding-for-disruptive-innovation-71d8532f2cf3",
   },
   {
     id: 26,
     content: "Product People, Mind the Gap! by Samuel Hulick",
-    link: "",
+    link: "https://ux.useronboard.com/product-people-mind-the-gap-da363018cc57",
   },
   {
     id: 27,
     content: "Research Questions Are Not Interview Questions by Erika Hall",
-    link: "",
+    link: "https://medium.com/mule-design/research-questions-are-not-interview-questions-7f90602eb533",
   },
   {
     id: 28,
     content:
       "Console Gaming Then and Now – 1997 Developer Interview by Gunpei Yokoi x Yukihito Morikawa",
-    link: "",
+    link: "http://shmuplations.com/yokoi/",
   },
   {
     id: 29,
     content: "Six Chasms in Need of Crossing by Anirudh Dhebar",
-    link: "",
+    link: "https://sloanreview.mit.edu/article/six-chasms-in-need-of-crossing/",
   },
   {
     id: 30,
     content: "Six Questions to Structure Great Projects by J.T. Trollman",
-    link: "",
+    link: "https://medium.com/facebook-design/six-questions-to-structure-great-projects-24a557cb11ab",
   },
   {
     id: 31,
     content: "Surviving enterprise UX: communicating value by Lisa Angela",
-    link: "",
+    link: "https://uxdesign.cc/surviving-enterprise-ux-part-1-6c37cdddf745",
   },
   {
     id: 32,
     content:
       "Surviving Enterprise UX: the importance of targeted user research by Lisa Angela",
-    link: "",
+    link: "https://uxdesign.cc/surviving-enterprise-ux-the-importance-of-targeted-user-research-c9823568ef1f",
   },
   {
     id: 33,
     content: "The 7 Deadly Sins of User Research by David Travis",
-    link: "",
+    link: "https://www.userfocus.co.uk/articles/7-sins-of-user-research.html",
   },
-  { id: 34, content: "The 9 Rules of Design Research by Erika Hall", link: "" },
+  {
+    id: 34,
+    content: "The 9 Rules of Design Research by Erika Hall",
+    link: "https://medium.com/mule-design/the-9-rules-of-design-research-1a273fdd1d3b",
+  },
   {
     id: 35,
     content: "The Complete Guide to the Kano Model by Folding Burritos",
-    link: "",
+    link: "https://foldingburritos.com/kano-model/",
   },
   {
     id: 36,
     content:
       "The Emotional Machine: Searching for Soul in the Autonomous Future",
-    link: "",
+    link: "https://medium.com/dford/the-emotional-machine-9de128e7af0f",
   },
   {
     id: 37,
     content: "The Future of UX Research by Monty Hammontree",
-    link: "",
+    link: "https://medium.com/microsoft-design/the-future-of-ux-research-14fe63743c1d",
   },
   {
     id: 38,
     content: "The History of Design Anthropology by Matt Artz",
-    link: "",
+    link: "https://medium.com/@matt_artz_anthropology/the-history-of-design-anthropology-ad04ce2792a",
   },
   {
     id: 39,
     content:
       "The lost art of injecting humanity into product design by Richard Bailey",
-    link: "",
+    link: "https://thenextweb.com/podium/2019/09/30/the-lost-art-of-injecting-humanity-into-product-design/",
   },
   {
     id: 40,
     content:
       "The New Enterprise Designer by Craig Mackintosh, American Express and Linda Quarles, frog",
-    link: "",
+    link: "https://www.frogdesign.com/designmind/the-new-enterprise-designer",
   },
   {
     id: 41,
     content: "The Next Century of Design Education by Albert Shum",
-    link: "",
+    link: "https://modus.medium.com/the-case-for-meta-modernist-design-5ca38ee7cc9d",
   },
-  { id: 42, content: "The Pyramid Principle by Ameet Ranadive", link: "" },
+  {
+    id: 42,
+    content: "The Pyramid Principle by Ameet Ranadive",
+    link: "https://medium.com/lessons-from-mckinsey/the-pyramid-principle-f0885dd3c5c7",
+  },
   {
     id: 43,
     content:
       "The Quintessential Guide For Building An Unforgettable First-time User Experience by Wayne Chang",
-    link: "",
+    link: "https://hackernoon.com/the-quintessential-guide-for-building-an-unforgettable-first-time-user-experience-19720a7447d2",
   },
   {
     id: 44,
     content:
       "The Top 5 Behavioural Economics Principles for Designers by Bridgeable",
-    link: "",
+    link: "https://uxplanet.org/the-top-5-behavioural-economics-principles-for-designers-ea22a16a4020",
   },
   {
     id: 45,
     content: "The Ultimate Guide to Customer Onboarding by Christina Perricone",
-    link: "",
+    link: "https://blog.hubspot.com/service/customer-onboarding",
   },
   {
     id: 46,
     content:
       "Uncover and make the most of your signature moments by Jess Leitch",
-    link: "",
+    link: "https://www.idean.com/spotlight/making-the-most-of-your-signature-moments",
   },
   {
     id: 47,
     content:
       "Understand and define users with the User Profile Model by Jessica Sherratt",
-    link: "",
+    link: "https://uxdesign.cc/understand-and-define-users-with-the-user-profile-model-f242047faaa9",
   },
   {
     id: 48,
     content:
       "Understanding your “aha” moments and putting them to work by Lynsey Duncan",
-    link: "",
+    link: "https://www.intercom.com/blog/understanding-your-aha-moments-and-putting-them-to-work/",
   },
-  { id: 49, content: "User research is a team sport by Myddelton", link: "" },
-  { id: 50, content: "What Autonomy Can Deliver by Jonah Houston", link: "" },
-  { id: 51, content: "What UX from 1989 can teach us by Dina Zuko", link: "" },
-  { id: 52, content: "When Coffee and Kale Compete by Vipul Bansal", link: "" },
+  {
+    id: 49,
+    content: "User research is a team sport by Myddelton",
+    link: "https://www.myddelton.co.uk/blog/user-research-is-a-team-sport",
+  },
+  {
+    id: 50,
+    content: "What Autonomy Can Deliver by Jonah Houston",
+    link: "https://medium.com/dford/what-autonomy-delivers-8e4b9a38bb61",
+  },
+  {
+    id: 51,
+    content: "What UX from 1989 can teach us by Dina Zuko",
+    link: "https://uxdesign.cc/what-ux-from-1989-can-teach-us-22476ac5703c",
+  },
+  {
+    id: 52,
+    content: "When Coffee and Kale Compete by Vipul Bansal",
+    link: "https://medium.com/@vipsyvipul/when-coffee-and-kale-compete-45e1155c26f1",
+  },
   {
     id: 53,
     content:
       "When Customers Want to See the Human Behind the Product by Adam Waytz",
-    link: "",
+    link: "https://hbr.org/2019/06/when-customers-want-to-see-the-human-behind-the-product",
   },
   {
     id: 54,
     content: "Why Highly Intelligent People Are Miserable by Michael Ndubuaku",
-    link: "",
+    link: "https://medium.com/swlh/why-highly-intelligent-people-are-miserable-c257f7d0680c",
   },
 ];
